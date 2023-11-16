a6802114ceabb95c3c46126ab6c316b88ac819d6
ito2370 <yitenghaixiang329@gmail.com>
2023-11-16 10:59:00 +0900

    テスト完了

--- MyGitProject2d/src/main/java/com/example/demo/MyGitProject2dApplication.java
+++ MyGitProject2d/src/main/java/com/example/demo/MyGitProject2dApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyGitProject2aApplication {
+public class MyGitProject2dApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyGitProject2aApplication.class, args);
+		SpringApplication.run(MyGitProject2dApplication.class, args);
 	}
 
 }
--- MyGitProject2d/src/test/java/com/example/demo/MyGitProject2dApplicationTests.java
+++ MyGitProject2d/src/test/java/com/example/demo/MyGitProject2dApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MyGitProject2aApplicationTests {
+class MyGitProject2dApplicationTests {
 
 	@Test
 	void contextLoads() {
